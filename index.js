9511939571cd1dc4687546555a8d6b912cd2cb8b
smh1988-2 <seamhurley1988@gmail.com>
2021-11-11 11:28:35 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+const BASE_URL = "https://anapioficeandfire.com/api/books"
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(BASE_URL)
+  .then((res) => res.json())
+  .then((books => renderBooks(books)))
 }
+
+let numOfPages = 0
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
+    numOfPages = numOfPages + (book["numberOfPages"])
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = book.name + ". Pages: " + book.numberOfPages;
     main.appendChild(h2);
+    console.log(numOfPages)
+
+    
   });
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
